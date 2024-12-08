f2cce04239e494ce98db655411ce1370521adb1e
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2024-12-08 08:20:34 +0600

    first commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+    return (
+        <div>
+            <h4 className="text-2xl">Plese Login</h4>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,10 @@
+
+const Register = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Please Register</h2>
+        </div>
+    );
+};
+
+export default Register;
